2d67d1376125f7462c1dd19ad49de94e30e20549
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2022-06-20 16:33:27 -0300

    added anime :heartbeat:

--- js/script.js
+++ js/script.js
@@ -12,3 +12,24 @@ menu.onclick = () => {
   menu.classList.toggle("fa-times");
   navbar.classList.toggle("active");
 };
+
+const target = document.querySelectorAll('[data-anime]');
+
+const animationClass = "animate";
+
+function animeScroll(){
+  const windowTop = window.pageYOffset + ((window.innerHeight * 3)/4.5);
+  target.forEach(function(element){
+    if(element.offsetTop < windowTop){
+      element.classList.add(animationClass)
+    }else{
+      element.classList.remove(animationClass)
+    }
+
+    // console.log(element.offsetTop)
+    // console.log(window.innerHeight)
+  })
+}
+window.addEventListener("scroll",function(){
+  animeScroll()
+})
